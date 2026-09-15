07d5523571659942113b0238d4cd61b4b78c029a
tievolutec-cloud <ti.evolutec@gmail.com>
2026-09-15 08:35:06 -0300

    feat: add login page

---
+++ app/login/page.tsx
@@ -0,0 +1,7 @@
+import LoginForm from "@/components/login-form";
+
+export const metadata = { title: "Entrar | Clínica Estelita" };
+
+export default function LoginPage() {
+  return <LoginForm />;
+}
