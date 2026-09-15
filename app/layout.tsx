99518525185b2f967c3bd2a9334c43e4e16985b9
tievolutec-cloud <ti.evolutec@gmail.com>
2026-09-15 10:30:27 -0300

    design: remove legacy green stylesheet and load final refinements

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import "./modern.css";
+import "./refinement.css";
 
 export const metadata: Metadata = {
   title: "Clínica Estelita",
